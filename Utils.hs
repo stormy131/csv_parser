37b4db80761d3a01fa215696ab76621ef91b0507
stormy131 <artemkopyl1990@gmail.com>
2024-06-13 21:40:59 +0200

    structure refactor + implemented main data processing

--- Utils.hs
+++ Utils.hs
@@ -1,6 +1,11 @@
 module Utils (
-    splitBy
+    splitBy,
+    findIndices,
+    deleteAt,
+    formatTable
 ) where
+
+import Data.List (elemIndex)
 
 splitBy :: String -> Char -> [String]
 splitBy [] _ = [""]
@@ -9,3 +14,16 @@ splitBy (x:xs) delim
     | otherwise = (x : head rest) : tail rest
     where
         rest = splitBy xs delim
+
+findIndices :: Eq a => [a] -> [a] -> [Int]
+findIndices [] _ = []
+findIndices (e:rest) list = idx : findIndices rest list
+    where Just idx = elemIndex e list
+
+deleteAt :: Int -> [a] -> [a]
+deleteAt 0 (_:rest) = rest
+deleteAt n (e:rest) = e : deleteAt (n - 1) rest
+
+formatTable :: Show a => [[a]] -> String
+formatTable = foldl (\ acc r -> acc ++ join r ++ "\n") ""
+    where join = foldl (\acc e -> acc ++ show e ++ "\t") ""
--- parser.hs
+++ parser.hs
@@ -1,26 +1,40 @@
 import System.IO
-import Utils (splitBy)
+import Data.List (sortBy)
+import Utils (
+       splitBy, 
+       findIndices, 
+       deleteAt, 
+       formatTable)
 
-predic :: String -> Bool
-predic _ = True
+type Query a = [Operator a]
+data Operator a = Select [String] | 
+                  Where String (a -> Bool) | 
+                  OrderBy String  | 
+                  Drop String
 
--- SQL Types
-data Query = Query Select (Where String) OrderBy
-data Select = SelectAll | Column Int
-data Where a = WhereA (a -> Bool) 
-data OrderBy = NoOrder | Key Int
+processQuery :: Query String -> [[String]] -> [[String]]
+processQuery [] table = table
+processQuery (first_op:rest) all_data@(columns:rows) = processQuery rest processed
+       where processed = case first_op of
+              Select names -> map (\row -> [row !! i | i <- column_ids]) all_data
+                     where column_ids = findIndices names columns
+              
+              Where column p -> columns : filter (p . (!! i)) rows
+                     where [i] = findIndices [column] columns
 
-processQuery :: Query -> [[String]] -> [[String]]
-processQuery (Query selection condition order) table =
-       case selection of
-              SelectAll-> table
-              Column id -> map (\row -> [row !! id]) table
+              OrderBy column -> columns : sortBy (\r1 r2 -> compare (r1 !! i) (r2 !! i)) rows
+                     where [i] = findIndices [column] columns
+
+              Drop column -> map (deleteAt i) all_data
+                     where [i] = findIndices [column] columns
+              
 
 -- Processing
 main = do
        handle <- openFile "test.csv" ReadMode
        contents <- hGetContents handle
-       let (columns:table) = map (`splitBy` ',') $ lines contents
-       let q = Query SelectAll (WhereA predic) NoOrder
+       let table = map (`splitBy` ',') $ lines contents
+       let q = [Select ["a", "c"], OrderBy "c"]
 
-       print $ processQuery q table
+       putStrLn $ formatTable table
+       putStr $ formatTable $ processQuery q table
